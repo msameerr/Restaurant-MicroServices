6a433f1b28e59f3481acfaa00e0cca5d98103adf
sameer arshad <arshadsameer820@gmail.com>
2024-11-01 06:41:57 +0500

    Add project files.

---
+++ delete.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace delete.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
